ad8673cd617c75fb9c16ddfc388d2c322aea34c6
tier5 <work@tier5.us>
2016-09-09 16:25:55 +0530

    added category dropdown on price page

--- resources/views/pages/price.blade.php
+++ resources/views/pages/price.blade.php
@@ -18,112 +18,148 @@
 <!-- ========================== -->
 <section class="features-section">
    <div class="container">
-      <div class="section-heading " >
-         <div class="section-title">Our Prices</div>
-         <div class="section-subtitle">
-            Our Master Craftsmen can do miracles--you will be amazed! <br>we offer full service Dry Cleaning, Green Cleaning (chemical free) and Wash & Fold , We also professionally clean Leather & Suede
+      <div class="row">
+         <div class="section-heading " >
+            <div class="section-title">Our Prices</div>
+            <div class="section-subtitle">
+               Our Master Craftsmen can do miracles--you will be amazed! <br>we offer full service Dry Cleaning, Green Cleaning (chemical free) and Wash & Fold , We also professionally clean Leather & Suede
+            </div>
+            <div class="design-arrow"></div>
          </div>
-         <div class="design-arrow"></div>
       </div>
-      <div class="col-md-6">
-         <ul class="plans">
-            <h2>Residential Service</h2>
-            @if(count($price_list) > 0)
-               @for($j=0 ; $j< count($price_list); $j++)
-                  @if($price_list[$j]->categories->name == 'Residential Services')
-                        <li class="plan highlight">
-                           <span class="price price-green">${{$price_list[$j]->price}}</span>
-                           <div class="details">
-                              <h1 class="plan-title" style="margin-top: 14px;">
-                                 <?php
-                                    $arr = 0;
-                                    if (preg_match('/.*\(.*\).*/', $price_list[$j]->item))
-                                    {
-                                      $arr = explode("(",$price_list[$j]->item);
-                                    }
-                                    else
-                                    {
-                                      echo $price_list[$j]->item;
-                                    }
-                                 ?>
-                                 {{$arr[0]}}
-                              </h1>
-                              <p>{{preg_match('/.*\(.*\).*/', $price_list[$j]->item )? substr_replace($arr[1], '(', 0, 0): ''}}</p>
+      <div class="row">
+         <div class="col-md-4">
+            <div class="plans">
+               <h2>Residential Service <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-up" aria-hidden="true"></i></h2>
+               <div class="product">
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
                            </div>
-                           <a href="{{route('getSignUp')}}"><button class="btn select ">Sign-Up Now</button></a>
-                        </li>
-                  @endif
-               @endfor
-            @else
-               No Data
-            @endif
-         </ul>
-      </div>
-      <div class="col-md-6">
-         <ul class="plans">
-            <h2>Household Items: Dry Clean</h2>
-            @if(count($price_list) > 0)
-               @foreach($price_list as $price)
-                  @if($price->categories->name == 'Household Items: Dry Clean')
-                     <li class="plan highlight">
-                        <span class="price price-green">${{$price->price}}</span>
-                        <div class="details">
-                           <h1 class="plan-title" style="margin-top: 14px;">
-                              <?php
-                                 $arr = 0;
-                                 if (preg_match('/.*\(.*\).*/', $price->item))
-                                 {
-                                   $arr = explode("(",$price->item);
-                                 }
-                                 else
-                                 {
-                                   echo $price->item;
-                                 }
-                              ?>
-                              {{$arr[0]}}
-                           </h1>
-                           <p>{{preg_match('/.*\(.*\).*/', $price->item )? substr_replace($arr[1], '(', 0, 0): ''}}</p>
                         </div>
-                        <a href="{{route('getSignUp')}}"><button class="btn select ">Sign-Up Now</button></a>
-                     </li>
-                  @endif
-               @endforeach
-            @else
-               No Data
-            @endif
-            <div style="height:40px;"></div>
-            <h2>Bedding</h2>
-            @if(count($price_list) > 0)
-               @foreach($price_list as $price)
-                  @if($price->categories->name == 'Bedding')
-                     <li class="plan highlight">
-                        <span class="price price-green">${{$price->price}}</span>
-                        <div class="details">
-                           <h1 class="plan-title" style="margin-top: 14px;">
-                              <?php
-                                 $arr = 0;
-                                 if (preg_match('/.*\(.*\).*/', $price->item))
-                                 {
-                                   $arr = explode("(",$price->item);
-                                 }
-                                 else
-                                 {
-                                   echo $price->item;
-                                 }
-                              ?>
-                              {{$arr[0]}}
-                           </h1>
-                           <p>{{preg_match('/.*\(.*\).*/', $price->item )? substr_replace($arr[1], '(', 0, 0): ''}}</p>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
                         </div>
-                        <a href="{{route('getSignUp')}}"><button class="btn select ">Sign-Up Now</button></a>
-                     </li>
-                  @endif
-               @endforeach
-            @else
-               No Data
-            @endif
-         </ul>
+                  </div>
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
+                           </div>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="plans">
+               <h2>Household Items <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-up" aria-hidden="true"></i></h2>
+               <div class="product">
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
+                           </div>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
+                        </div>
+                  </div>
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
+                           </div>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <div class="col-md-4">
+            <div class="plans">
+               <h2>Bedding <i class="fa fa-caret-down" aria-hidden="true"></i><i class="fa fa-caret-up" aria-hidden="true"></i></h2>
+               <div class="product">
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
+                           </div>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
+                        </div>
+                  </div>
+                  <div class="product-section">
+                        <div class="col-md-3">
+                           <img src="http://localhost/html/urang/public/images/demo.png" class="img-responsive">
+                        </div>
+                        <div class="col-md-6">
+                           <div class="row">
+                              <h3>product name</h3>
+                           </div>
+                        </div>
+                        <div class="col-md-3">
+                           <div class="row">
+                              <h3>$40.00</h3>
+                           </div>
+                        </div>
+                  </div>
+               </div>
+            </div>
+         </div>
       </div>
    </div>
 </section>
 @endsection
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script type="text/javascript">
+   $(document).ready(function(){
+      $(".plans h2").click(function(){
+         var product_box = $(this).parent().find($('.product'));
+         product_box.toggleClass("close");
+         if(product_box.hasClass("close")){
+            product_box.slideUp(100);
+            $(this).find($('.fa.fa-caret-up')).hide();
+            $(this).find($('.fa.fa-caret-down')).show();
+            
+         }
+         else{
+            product_box.slideDown(100);
+            $(this).find($('.fa.fa-caret-up')).show();
+            $(this).find($('.fa.fa-caret-down')).hide();
+         }
+      });
+   });
+</script>
