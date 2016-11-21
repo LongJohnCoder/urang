d871126b6c626570f9d27c2d8d77ae493ced273d
tier511 <work@tier5.us>
2016-11-21 17:22:45 +0530

    customer list issue fix v0.2

--- app/ref.php
+++ app/ref.php
@@ -9,4 +9,7 @@ class ref extends Model
     public function user() {
     	return $this->hasOne('App\User', 'id', 'user_id');
     }
+    public function userDetails() {
+    	return $this->hasOne('App\UserDetails', 'user_id', 'user_id');
+    }
 }
