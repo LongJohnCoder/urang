498167bd38beca68502566be25f510efd724d7b6
tier511 <work@tier5.us>
2016-12-20 14:35:50 +0530

    Google Analytics v1.0

--- resources/views/pages/includes/stand-alone-header.blade.php
+++ resources/views/pages/includes/stand-alone-header.blade.php
@@ -55,3 +55,12 @@
 <script type="text/javascript" src="{{url('/')}}/public/new/js/jquery.jscrollpane.min.js"></script>
 <!--time js-->
 <script type="text/javascript" src="{{url('/')}}/public/js/time-picker/jquery.timepicker.min.js"></script>
+<script>
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-74654877-5', 'auto');
+	ga('send', 'pageview');
+</script>
